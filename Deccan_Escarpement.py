b4abb04e8c3b26646f5577e34c9f5ecd449290e5
Pons Michaël <mikpons2@gmail.com>
2018-06-28 21:33:17 +0200

    last

--- Deccan_Escarpement.py
+++ Deccan_Escarpement.py
@@ -32,15 +32,58 @@ for i in range(1,len(latprof)):
     k=np.nonzero(lat==latprof[i])
     profiles2=elevation[k]
     profiles=np.concatenate((profiles,profiles2))
-    print(profiles)
-    print('hello')
+##    print(profiles)
+##    print('hello')
     
 profiles3=np.reshape(profiles,(len(latprof),np.shape(k)[1]))
-plt.pcolormesh(profiles3)
-plt.show()
 
-##profiles3=np.asmatrix(profiles3)
-##plt.plot(profiles3)
+profmat=np.asmatrix(profiles3)
+
+M=[]
+indmax=[]
+elev=[]
+
+elev=np.amax(profiles3, axis=1)
+
+##l=profmat.max(axis=1)
+
+
+for i in range(len(latprof)):
+    l=np.nonzero(profiles3[i]==elev[i])[0][0] #YESS Found the mistake a 0 was missing Wohoo!
+    M.append(l)
+
+count,division=np.histogram(M,bins=20)
+##plt.hist(M, bins=20)
 ##plt.show()
 
-    
+E=np.nonzero(count==max(count))[0][0]
+diffindX=abs(M-count[E])
+
+tolerance=65
+mintol=round(count[E]-tolerance)
+maxtol=round(count[E]+tolerance)
+
+prof=[]
+indmax2=[]
+G=[]
+
+for i in range(len(latprof)):
+    if diffindX[i]>tolerance:
+        prof=profiles3[i,mintol:maxtol]
+        G=np.nonzero(profiles3[i]==max(prof))[0][0]
+        indmax2.append(G)
+    else:
+        indmax2.append(M[i]);
+        
+diffindX2=abs(indmax2-count[E])
+
+
+vect=np.linspace(0,len(latprof),len(latprof))
+vect=np.round(vect)
+plt.pcolormesh(profiles3,cmap='RdBu_r',shading='flat')
+plt.plot(indmax2,vect,'b')
+plt.title('Finding coastal escarpement, example:Western Ghats-India')
+plt.show()
+
+
+
